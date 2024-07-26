d6c05ca7f8e2c9596361445da0811aa06d943168
Aditi Radhakisan Kute <108111520+aditikute24@users.noreply.github.com>
2024-07-26 13:32:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,3 +11,13 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+document.querySelector('.right-contact form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Perform any additional actions here, e.g., validation, custom API call, etc.
+
+    // After custom actions, you can submit the form programmatically if needed
+    event.target.submit();
+});
+
